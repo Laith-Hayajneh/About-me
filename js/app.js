0cf00bad0e692461862ba9d4c93a587357856a3d
laith-hayajneh <farfesh96@gmail.com>
2021-04-27 18:11:08 +0300

    lab03

--- js/app.js
+++ js/app.js
@@ -1,43 +1,99 @@
-'use strict '
-let vistor =prompt('Please enter your name ');
-alert('welcome ' + vistor + ' now i will ask you some question about me to how much you realy now me ');
-let q1 =prompt('The owner of this site is Laith , Yes OR No').toLowerCase();
-console.log (q1)
-if (q1 =="yes" || q1 =='y'){
-    alert('well done ');
+// 'use strict '
+// let vistor =prompt('Please enter your name ');
+// alert('welcome ' + vistor + ' now i will ask you some question about me to how much you realy now me ');
+// let q1 =prompt('The owner of this site is Laith , Yes OR No').toLowerCase();
+// console.log (q1)
+// if (q1 =="yes" || q1 =='y'){
+//     alert('well done ');
     
-}else{
-    alert('Sorry , you need to search again ');
-};
-let q2 =prompt('Do i like BMW cars, yes OR no ').toLowerCase();
-console.log (q2)
-if (q2 =="yes" || q2 =='y'){
-    alert(' wait you will see my favorite one ');
-}else{
-    alert('Sorry , but i love BMW cars ');
-};
-let q3 =prompt('How old am i 23 or not ').toLowerCase();
-console.log (q3)
-if (q3 =="yes" || q3 =='y'){
-    alert('  I appreciate that you know my age  ');
+// }else{
+//     alert('Sorry , you need to search again ');
+// };
+// let q2 =prompt('Do i like BMW cars, yes OR no ').toLowerCase();
+// console.log (q2)
+// if (q2 =="yes" || q2 =='y'){
+//     alert(' wait you will see my favorite one ');
+// }else{
+//     alert('Sorry , but i love BMW cars ');
+// };
+// let q3 =prompt('How old am i 23 or not ').toLowerCase();
+// console.log (q3)
+// if (q3 =="yes" || q3 =='y'){
+//     alert('  I appreciate that you know my age  ');
     
-}else{
-    alert('you will not invite to my birthday hahaha');
-};
-let q4 =prompt('Do i like mansaf ').toLowerCase();
-console.log (q4)
-if (q4 =="yes" || q4 =='y'){
-    alert('it\'s my favorite one  ');
+// }else{
+//     alert('you will not invite to my birthday hahaha');
+// };
+// let q4 =prompt('Do i like mansaf ').toLowerCase();
+// console.log (q4)
+// if (q4 =="yes" || q4 =='y'){
+//     alert('it\'s my favorite one  ');
     
-}else{
-    alert('Are you sure ,There is no one dosen\'s like mansaf ');
-};
-let q5 =prompt('Did i graduated from JUST university').toLowerCase();
-console.log (q5)
-if (q5 =="yes" || q5 =='y'){
-    alert('NO you are wrong it\'s YARMOUK ');
+// }else{
+//     alert('Are you sure ,There is no one dosen\'s like mansaf ');
+// };
+// let q5 =prompt('Did i graduated from JUST university').toLowerCase();
+// console.log (q5)
+// if (q5 =="yes" || q5 =='y'){
+//     alert('NO you are wrong it\'s YARMOUK ');
     
-}else{
-    alert('That is great  ');
-};
-alert('Thank you  ' + vistor + ' ,now i will show you the true fact about me  ');
+// }else{
+//     alert('That is great  ');
+// };
+// alert('Thank you  ' + vistor + ' ,now i will show you the true fact about me  ');
+
+// let totalAttemptsQ6 = 4;
+ let min = 1;
+// while( totalAttemptsQ6 !=0){
+// let univ = Number( prompt('What do you think i stay study in UNIVERSITY'))
+
+// if (univ == 4){
+//     alert('Exactly , you are right  ')
+//     break;
+// }else if (univ > 4) {
+//     alert('too high')
+//     totalAttemptsQ6 = totalAttemptsQ6 - min;
+// }else if(univ < 4){
+//     alert ('too low');
+//     totalAttemptsQ6 = totalAttemptsQ6 - min;
+
+// }else {
+//     alert('you must enter a number ');
+//     totalAttemptsQ6 = totalAttemptsQ6 - min;
+
+// }}
+
+// if (totalAttemptsQ6 == 0) {
+
+//     alert("Maximum number of attempts exceeded,The correct answer is 4");
+// }
+
+
+
+
+
+
+// Quistion 7
+let car = ['kia', 'bmw', 'ford', 'mercedes'];
+// while(totalAttemptsQ7 != 0){
+let userAnswer = prompt('what is my fav car?');
+ for (let i = 1; i <=6; i++) {
+     if (car.includes(userAnswer)) {
+        console.log('yes that\'s one of them');
+        break;
+        
+    }else{
+        alert('sorry it is not one them ');
+        userAnswer=prompt('what is my fav car?');
+    }else if(i >6){
+        alert("This is the answer 'kia', 'bmw', 'ford', 'mercedes'")
+    }
+ }
+
+
+ 
+
+
+
+
+
