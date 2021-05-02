3f7418671bdb72ed8d027d7dd9e2c72f4defe237
laith-hayajneh <farfesh96@gmail.com>
2021-05-02 21:55:33 +0300

    removing the console

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ let vistor = prompt('Please enter your name ');
 alert('welcome ' + vistor + ' now i will ask you some question about me to how much you realy now me ');
 function question1() {
     let q1 = prompt('The owner of this site is Laith , Yes OR No').toLowerCase();
-    console.log(q1)
     if (q1 == "yes" || q1 == 'y') {
         alert('well done ');
         score = score + 1;
@@ -16,7 +15,6 @@ question1();
 function question2()
 {
 let q2 = prompt('Do i like BMW cars, yes OR no ').toLowerCase();
-console.log(q2)
 if (q2 == "yes" || q2 == 'y') {
     alert(' wait you will see my favorite one ');
     score = score + 1;
@@ -28,7 +26,6 @@ question2();
 function question3()
 {
 let q3 = prompt('How old am i 23 or not ').toLowerCase();
-console.log(q3)
 if (q3 == "yes" || q3 == 'y') {
     alert('  I appreciate that you know my age  ');
     score = score + 1;
@@ -40,7 +37,6 @@ question3();
 function question4()
 {
 let q4 = prompt('Do i like mansaf ').toLowerCase();
-console.log(q4)
 if (q4 == "yes" || q4 == 'y') {
     alert('it\'s my favorite one  ');
     score = score + 1;
@@ -52,7 +48,6 @@ question4();
 function question5()
 {
 let q5 = prompt('Did i graduated from JUST university').toLowerCase();
-console.log(q5)
 if (q5 == "yes" || q5 == 'y') {
     alert('NO you are wrong it\'s YARMOUK ');
 
@@ -116,5 +111,4 @@ for (let i = 1; i <= 6; i++) {
 };
 alert("This is the answers: kia, bmw, ford, mercedes");
 }
-alert('your final score is  ' + score)
-console.log(score)
+alert('your final score is  ' + score);
